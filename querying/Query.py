c990e243ca73220f45ab479202e97847e82f628f
Brody Critchlow <brodycritchlow@gmail.com>
2025-08-02 18:19:10 -0400

    feat: add query stubs

---
+++ querying/Query.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from common.Types import Component, Entity
+from containers.ComponentStorage import ComponentStorage
+
+class Query(object):
+    def with_components(self, *types: type[Component]) -> Query:
+        # TODO: Implement proper query condition logic
+        ...
+    
+    def without_components(self, *types: type[Component]) -> Query:
+        # TODO: Implement proper query condition logic
+        ...
+
+    def execute(self, storage: ComponentStorage) -> list[Entity]:
+        # TODO: Implement proper query execution logic
+        ...
